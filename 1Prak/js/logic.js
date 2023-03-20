12ebae02bccb454299d6fd62e64142b84fa9fe5d
abazatte <a.azattemuer@gmail.com>
2023-03-20 15:53:51 +0100

    feat: modal funktioniert jetzt

--- 1Prak/js/logic.js
+++ 1Prak/js/logic.js
@@ -23,12 +23,7 @@ function backToTop() {
   document.documentElement.scrollTop = 0;
 }
 
-myModal.click('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-title').text('New message to ' + recipient)
-  modal.find('.modal-body input').val(recipient)
-})
+$("#brotForm").submit(function( event ) {
+  alert($('#titel').val());
+  event.preventDefault();
+});
