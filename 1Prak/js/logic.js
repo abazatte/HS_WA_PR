345b68948a1ac24339e54cba188438fb33efb7c9
maxono1 <92861475+mjaesch@users.noreply.github.com>
2023-03-24 16:56:58 +0100

    feat: validation hinzugefügt
    normale validation in der html, in der js wird bootstrap klasse "was-validated" verwendet

--- 1Prak/js/logic.js
+++ 1Prak/js/logic.js
@@ -24,24 +24,33 @@ function backToTop() {
 }
 
 function addRow() {
-  let id = document.getElementById("id").value;
-  let name = document.getElementById("name").value;
-  let year = document.getElementById("year").value;
-  let art = document.getElementById("art").value;
-  let vegan = document.getElementById("vegan").checked;
-  let gluten = document.getElementById("gluten").checked;
-  let table = document.getElementById("brotTable");
-  let row = table.insertRow();
-  let cell1 = row.insertCell(0);
-  let cell2 = row.insertCell(1);
-  let cell3 = row.insertCell(2);
-  let cell4 = row.insertCell(3);
-  let cell5 = row.insertCell(4);
-  let cell6 = row.insertCell(5);
-  cell1.innerHTML = id;
-  cell2.innerHTML = name;
-  cell3.innerHTML = year;
-  cell4.innerHTML = art;
-  cell5.innerHTML = vegan ? "✔️" : "❌";
-  cell6.innerHTML = gluten ? "✔️" : "❌";
+  //unser formular
+  const form = document.getElementById("brotForm");
+  form.classList.add('was-validated')
+
+  if (form.checkValidity()) {
+    let id = document.getElementById("id").value;
+    let name = document.getElementById("name").value;
+    let year = document.getElementById("year").value;
+    let art = document.getElementById("art").value;
+    let vegan = document.getElementById("vegan").checked;
+    let gluten = document.getElementById("gluten").checked;
+    let table = document.getElementById("brotTable");
+    let row = table.insertRow();
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
+    let cell6 = row.insertCell(5);
+    cell1.innerHTML = id;
+    cell2.innerHTML = name;
+    cell3.innerHTML = year;
+    cell4.innerHTML = art;
+    cell5.innerHTML = vegan ? "✔️" : "❌";
+    cell6.innerHTML = gluten ? "✔️" : "❌";
+  }
+
+
+
 }
