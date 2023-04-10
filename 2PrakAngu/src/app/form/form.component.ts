a45cb0fe2d7d443a777cdd2e9fbe8eb4d2f91379
mjaesch <maximilianjaesch@gmail.com>
2023-04-10 07:21:57 -0400

    feat: json wird geladen

--- 2PrakAngu/src/app/form/form.component.ts
+++ 2PrakAngu/src/app/form/form.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import BrotJson from './form_init.json';
+
+interface Brot {
+  id : Number;
+  name : String;
+  year : Number;
+  type : String;
+  vegan : boolean;
+  glutenFree : boolean;
+}
 
 @Component({
   selector: 'app-form',
@@ -6,5 +16,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.sass', '../css/style.css']
 })
 export class FormComponent {
-
+  brote: Brot[] = BrotJson;
+  constructor(){
+    console.log(this.brote);
+  }
 }
