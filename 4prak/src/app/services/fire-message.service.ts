962df604da7252b80c47e9949bc8b67cc44517d0
abazatte <a.azattemuer@gmail.com>
2023-05-12 07:10:22 -0400

    clean: bisschen gecleaned

--- 4prak/src/app/services/fire-message.service.ts
+++ 4prak/src/app/services/fire-message.service.ts
@@ -40,9 +40,9 @@ export class FireMessageService {
 
   public async addMessage(author: string, text: string) {
     const docRef = await addDoc(collection(this.firestore, environment.room), {
-      'author': author,
-      'text': text,
-      'timestamp': serverTimestamp()
+      author,
+      text,
+      timestamp: serverTimestamp()
     })
   }
 
