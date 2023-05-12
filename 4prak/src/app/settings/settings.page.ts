2656e3fdc74d93da496a14c36538aba388938c3a
mjaesch <maximilianjaesch@gmail.com>
2023-05-12 09:24:44 -0400

    feat: alert anzeigen bei vergebenen namen!!!

--- 4prak/src/app/settings/settings.page.ts
+++ 4prak/src/app/settings/settings.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FireMessageService } from '../services/fire-message.service';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
@@ -9,16 +9,25 @@ import { FireMessageService } from '../services/fire-message.service';
 export class SettingsPage {
   username: string;
   actualUsername: string;
-  constructor(private firemessage: FireMessageService) { 
+  constructor(private firemessage: FireMessageService, private alertController: AlertController) { 
     this.username = '';
     this.actualUsername = this.firemessage.getUsername();
   }
+
+  
+  
 
   async sendUsername() {
     if(await this.firemessage.setUsername(this.username)){
       this.actualUsername = this.firemessage.getUsername();
     } else {
-      //show alert, name bereits vergeben
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        message: 'Name bereits vergeben',
+        buttons: ['OK'],
+      });
+  
+      await alert.present();
     }
     
   }
